189475e2da8fdc274f8920c352b933e451f3f582
Alfre Moracho <alfrevp@hotmail.com>
2022-03-25 03:41:56 -0300

    Modifico archivo y agrego README

---
+++ README.md
@@ -0,0 +1,2 @@
+Nombre: Alfredo Moracho
+num alumno: 15080/8
--- adivino.py
+++ adivino.py
@@ -1,16 +1,16 @@
-## Adivina adivinador....
+## Adivina adivinador...
 import random
-numero_aleatorio = random.randrange(5)
+numero_aleatorio = random.randrange(101)
 gane = False
-print("Tenés 3 intentos para adivinar un entre 0 y 99")
+print ("Tenés 5 intentos para adivinar un número entre 0 y 99")
 intento = 1
-while intento < 4 and not gane:
-    numeroIngresado = int(input('Ingresa tu número: '))
-    if numeroIngresado == numero_aleatorio:
-        print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
+while intento < 6 and not gane:
+    numero_ingresado = int(input("Ingresa tu número: "))
+    if numero_ingresado == numero_aleatorio:
+        print ("Ganaste! y necesitaste {} intentos!!!".format(intento))
         gane = True
     else:
-        print('Mmmm ... No.. ese número no es... Seguí intentando.')
+        print ("Mmmm... No... Ese número no es... Seguí intentando.")
         intento += 1
 if not gane:
-    print('\n Perdiste :(\n El número era: {}'.format(numero_aleatorio))
+    print ("\ Perdiste :(\n El número era: {}".format(numero_aleatorio))
